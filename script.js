0f5cfa82d5cf4d53f7ba291f933ffaa8d76107bd
Solmyr <solmyr200@gmail.com>
2024-05-22 13:53:35 +0200

    Got source

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // element-one: Dupla kattintásra hozzáad egy "animation" class-t kattintáskor
+    const elementOne = document.getElementById('element-one');
+
+    // element-two: Ha rámegy az egér, hozzáad egy box-shadow-t
+    const elementTwo = document.getElementById('element-two');
+
+    // element-three: Kattintásra eltűnik
+    const elementThree = document.getElementById('element-three');
+
+    // element-four: Kattintásra a háttere zöld lesz
+    const elementFour = document.getElementById('element-four');
+    
+});
