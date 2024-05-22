3f578b542dd566a985502d9e779462d6a648f46f
Solmyr <solmyr200@gmail.com>
2024-05-22 13:59:18 +0200

    Js complete

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     // element-one: Dupla kattintásra hozzáad egy "animation" class-t kattintáskor
     const elementOne = document.getElementById('element-one');
+    elementOne.addEventListener("dblclick", () => {
+        elementOne.classList.add("animation");
+    });
 
     // element-two: Ha rámegy az egér, hozzáad egy box-shadow-t
     const elementTwo = document.getElementById('element-two');
+    elementTwo.addEventListener("mouseover", () => {
+        elementTwo.style.boxShadow = "5px 10px";
+    });
 
     // element-three: Kattintásra eltűnik
     const elementThree = document.getElementById('element-three');
+    elementThree.addEventListener("click", () => {
+        elementThree.style.visibility = "hidden";
+    });
 
     // element-four: Kattintásra a háttere zöld lesz
     const elementFour = document.getElementById('element-four');
-    
+    elementFour.addEventListener("click", () => {
+        elementFour.style.backgroundColor = "green";
+    });
 });
